8551d15dc88051f2b43dab54eef6585602feddfd
Samiran Roy <samiranrl@users.noreply.github.com>
2021-07-15 12:19:47 +0530

    Update pandas_starter.py

--- scripts/pandas_starter.py
+++ scripts/pandas_starter.py
@@ -69,3 +69,24 @@ b = date(2009, 6, 9)
 
 for dt in rrule(DAILY, dtstart=a, until=b):
     print dt.strftime("%Y-%m-%d")
+
+    
+    
+# quantile/decile
+
+# importing the modules
+import pandas as pd
+import numpy as np
+    
+# creating a DataFrame
+df = {'Name' : ['Amit', 'Darren', 'Cody', 'Drew',
+                'Ravi', 'Donald', 'Amy'],
+      'Score' : [50, 71, 87, 95, 63, 32, 80]}
+df = pd.DataFrame(df, columns = ['Name', 'Score'])
+  
+# adding Decile_rank column to the DataFrame
+df['Decile_rank'] = pd.qcut(df['Score'], 10,
+                            labels = False)
+  
+# printing the DataFrame
+print(df)
